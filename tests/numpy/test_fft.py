597d977316f1cf443cc090dcb47e6014c0538ed5
ETumang <emily.tumang@gmail.com>
2014-04-17 02:16:01 -0400

    Now with extra saving things in their own subfolder+holding on to read in data! TODO: hold on to FFT results and timesteps.

--- tests/numpy/test_fft.py
+++ tests/numpy/test_fft.py
@@ -2,6 +2,7 @@ import scipy.io.wavfile as wavf
 import numpy.fft as fourier
 import numpy
 import matplotlib.pyplot as plt
+import os
 
 PI = numpy.pi
 
@@ -11,6 +12,11 @@ interval = .1 #must be <1
 def get_freqs(file):
 	rate, data = wavf.read(file)
 	sig = (str(file).split('.'))[0]+"_"
+	dirname = sig+'data'
+	path = dirname+'/'
+	if not os.path.exists(dirname):os.mkdir(dirname)
+
+	fft_data = []
 
 	numstep = rate * interval
 
@@ -24,15 +30,20 @@ def get_freqs(file):
 		img = a.imag
 		freqs =abs(fourier.fftfreq(len(a),8000)/(2*PI))
 
-		plt.semilogy(freqs,mag,'b.')
-		plt.ylim([10^-1,10^7])
+		#plt.semilogy(freqs,mag,'b.')
+		#plt.ylim([10^-1,10^7])
 
 		name = sig+str(i)
 
-		plt.savefig(name+'.png')
+		#plt.savefig(path+name+'.png')
 		#plt.show()
 		plt.clf()
+		fft_data.append(a)
 
+	print fft_data
+
+	numpy.save(path+'data',data)
+	numpy.append(path+'fft_results', fft_data)
 		
 get_freqs(f)
 
