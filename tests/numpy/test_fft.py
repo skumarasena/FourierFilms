eb28d0dd8055431ec79878b4f263291eb7e1f72d
ETumang <emily.tumang@gmail.com>
2014-04-19 10:57:33 -0400

    Code frozen on test_fft. Doc 'Run' includes instructions to create movie with this codeset.

--- tests/numpy/test_fft.py
+++ tests/numpy/test_fft.py
@@ -3,13 +3,12 @@ import numpy.fft as fourier
 import numpy
 import matplotlib.pyplot as plt
 import os
-import example2.py
 
 PI = numpy.pi
 
 #todo: shift things, filter things
 
-f = 'piano.wav'#replace this with the filepath of your desired sound
+f = 'music.wav'#replace this with the filepath of your desired sound
 interval = .1 #must be <1
 
 def get_freqs(file):
@@ -31,8 +30,8 @@ def get_freqs(file):
 
 		a = fourier.fft(data[start:stop])
 		mag = numpy.absolute(a[1:])
-		img = a.imag
 		freqs =abs(fourier.fftfreq(len(a[1:]),1.0/rate))
+		angles = numpy.angle(a[1:])
 
 		plt.plot(freqs,mag,'b.')
 		plt.ylim([0,30000])# find a way to set this that isn't arbitrary...
@@ -40,13 +39,12 @@ def get_freqs(file):
 		name = sig+str(i)
 
 		plt.savefig(path+name+'.png')
+		numpy.save(path+'freq'+str(i),freqs)
 		#plt.show()
 		plt.clf()
-		fft_data.append(a)
 
-	numpy.save(path+'data',data)
 	#todo: put a copy of the .wav in the new folder, save the frequencies and amplitudes
 	return dirname
 
 
-
+get_freqs(f)
